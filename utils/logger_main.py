a0c0740ebcf1e7117daf4d9ab20d2aed3750f076
Arce <22849351+Senas23@users.noreply.github.com>
2022-03-22 10:55:35 -0500

    Update logger_main.py

--- utils/logger_main.py
+++ utils/logger_main.py
@@ -4,7 +4,7 @@ import os
 if not os.path.exists(f"{os.getcwd()}/logs"):
     os.mkdir(f"{os.getcwd()}/logs")
 
-logging.basicConfig(level=logging.DEBUG, filename='./logs/export_cp_to_ansible.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG, filename='./logs/export.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
 console.setFormatter(logging.Formatter("%(levelname)s %(message)s"))
